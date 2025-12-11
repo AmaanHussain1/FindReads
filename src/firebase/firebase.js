0d4f84335f60a56c763386810fce018e67311c20
user.name "Amaan <user.email "amaanhussain1706@gmail.com>
2025-12-12 01:07:42 +0530

    Update Firebase Auth

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // You can find this in your Firebase Console -> Project Settings -> General -> "Your apps"
 const firebaseConfig = {
-  apiKey: "AIzaSyBqFp3HbGw0uEAvnwRi9biLdRd39TQdDUQ",
-  authDomain: "book-search-app-1b983.firebaseapp.com",
-  projectId: "book-search-app-1b983",
-  storageBucket: "book-search-app-1b983.firebasestorage.app",
-  messagingSenderId: "786790626002",
-  appId: "1:786790626002:web:7995b64ef40a573767b30b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
